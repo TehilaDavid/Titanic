26fc28f87aaf86266719004d18f4d68931f37e6b
Tehila <Tehila>
2022-05-27 00:25:07 +0300

    26/5

--- src/main/java/Constants.java
+++ src/main/java/Constants.java
@@ -2,8 +2,8 @@ public class Constants {
     public static final String PATH_TO_DATA_FILE = "src/main/resources/titanic.csv";
     public static final String PATH_TO_FILTERED_FILE = "C:\\Users\\tehil\\Downloads\\";
 
-    public static final int WINDOW_WIDTH = 600;
-    public static final int WINDOW_HEIGHT = 700;
+    public static final int WINDOW_WIDTH = 900;
+    public static final int WINDOW_HEIGHT = 550;
 
     public static final String[] PASSENGER_CLASS_OPTIONS = {"All", "1st", "2nd", "3rd"};
     public static final String[] PASSENGER_SEX_OPTIONS = {"All", "Female", "Male"};
@@ -14,15 +14,18 @@ public class Constants {
 
     public static final int SPACE = 5;
 
-    public static final int LABEL_WIDTH = 250;
+    public static final int LABEL_WIDTH = 400;
     public static final int LABEL_HEIGHT = 30;
 
     public static final int COMBO_BOX_WIDTH = 80;
     public static final int COMBO_BOX_HEIGHT = 30;
 
-    public static final int BUTTON_WIDTH = 100;
-    public static final int BUTTON_HEIGHT = 30;
+    public static final int BUTTON_WIDTH = 150;
+    public static final int BUTTON_HEIGHT = 60;
 
     public static final int TEXT_FIELD_WIDTH = 130;
     public static final int TEXT_FIELD_HEIGHT = 30;
+
+    public static final int LABEL_FONT_SIZE = 22;
+    public static final int BUTTON_FONT_SIZE = 30;
 }
--- src/main/java/MainPanel.java
+++ src/main/java/MainPanel.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
@@ -35,13 +36,19 @@ public class MainPanel extends JPanel {
     private int filterCounter;
     private String firstLine;
     private int x;
+    private Font labelFont;
+    private Font buttonFont;
+
+    private ImageIcon background;
 
 
     public MainPanel(int x, int y, int width, int height) {
-        File file = new File(Constants.PATH_TO_DATA_FILE); //this is the path to the data file
+        File file = new File(Constants.PATH_TO_DATA_FILE);
         this.setLayout(null);
         this.setBounds(x, y + Constants.MARGIN_FROM_TOP, width, height);
         this.filterCounter = 0;
+        this.labelFont = new Font("David",Font.BOLD,Constants.LABEL_FONT_SIZE);
+        this.buttonFont = new Font("David",Font.BOLD,Constants.BUTTON_FONT_SIZE);
 
         createPassengerList(file);
         this.x = x + Constants.MARGIN_FROM_LEFT;
@@ -50,6 +57,12 @@ public class MainPanel extends JPanel {
 
         filtering();
 
+    }
+
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        this.background = new ImageIcon("titanicImage.jpg");
+        this.background.paintIcon(this, g, 0,0);
     }
 
     private void createPassengerList(File file) {
@@ -74,19 +87,25 @@ public class MainPanel extends JPanel {
     private void createFilters(int y) {
         JLabel idMinLabel = new JLabel("Min passenger Id: ");
         idMinLabel.setBounds(this.x, y, Constants.LABEL_WIDTH, Constants.LABEL_HEIGHT);
+        idMinLabel.setFont(this.labelFont);
+        idMinLabel.setForeground(Color.WHITE);
         this.add(idMinLabel);
 
         this.idMinTextField = new JTextField();
         this.idMinTextField.setBounds(idMinLabel.getX() + idMinLabel.getWidth(), y, Constants.TEXT_FIELD_WIDTH, Constants.TEXT_FIELD_HEIGHT);
+        idMinTextField.setFont(this.labelFont);
         this.add(this.idMinTextField);
 
         JLabel idMaxLabel = new JLabel("Max passenger Id: ");
         idMaxLabel.setBounds(this.x, y + Constants.LABEL_HEIGHT + Constants.SPACE, Constants.LABEL_WIDTH, Constants.LABEL_HEIGHT);
+        idMaxLabel.setFont(this.labelFont);
+        idMaxLabel.setForeground(Color.WHITE);
         this.add(idMaxLabel);
 
 
         this.idMaxTextField = new JTextField();
         this.idMaxTextField.setBounds(idMaxLabel.getX() + Constants.LABEL_WIDTH, this.idMinTextField.getY() + Constants.LABEL_HEIGHT + Constants.SPACE, Constants.TEXT_FIELD_WIDTH, Constants.TEXT_FIELD_HEIGHT);
+        idMaxTextField.setFont(this.labelFont);
         this.add(this.idMaxTextField);
 
         createPClassFilter();
@@ -106,11 +125,15 @@ public class MainPanel extends JPanel {
     private void createPClassFilter() {
         JLabel pClassLabel = new JLabel("Passenger class: ");
         pClassLabel.setBounds(this.x, this.idMaxTextField.getY() + Constants.LABEL_HEIGHT + Constants.SPACE, Constants.LABEL_WIDTH, Constants.LABEL_HEIGHT);
+        pClassLabel.setFont(this.labelFont);
+        pClassLabel.setForeground(Color.WHITE);
         this.add(pClassLabel);
 
         this.pClassComboBox = new JComboBox<>(Constants.PASSENGER_CLASS_OPTIONS);
         this.pClassComboBox.setBounds(pClassLabel.getX() + pClassLabel.getWidth(), pClassLabel.getY(), Constants.COMBO_BOX_WIDTH, Constants.COMBO_BOX_HEIGHT);
+        pClassComboBox.setFont(this.labelFont);
         this.add(this.pClassComboBox);
+
         this.pClassComboBox.addActionListener((e) -> {
             this.classType = this.pClassComboBox.getSelectedIndex();
         });
@@ -119,21 +142,28 @@ public class MainPanel extends JPanel {
     private void createNameFilter() {
         JLabel nameLabel = new JLabel("Passenger name: ");
         nameLabel.setBounds(this.x, this.pClassComboBox.getY() + Constants.COMBO_BOX_HEIGHT + Constants.SPACE, Constants.LABEL_WIDTH, Constants.LABEL_HEIGHT);
+        nameLabel.setFont(this.labelFont);
+        nameLabel.setForeground(Color.WHITE);
         this.add(nameLabel);
 
         this.nameTextField = new JTextField();
         this.nameTextField.setBounds(nameLabel.getX() + nameLabel.getWidth(), nameLabel.getY(), Constants.TEXT_FIELD_WIDTH, Constants.TEXT_FIELD_HEIGHT);
+        nameTextField.setFont(this.labelFont);
         this.add(this.nameTextField);
     }
 
     private void createSexFilter() {
         JLabel sexLabel = new JLabel("Passenger sex: ");
         sexLabel.setBounds(this.x, this.nameTextField.getY() + Constants.LABEL_HEIGHT + Constants.SPACE, Constants.LABEL_WIDTH, Constants.LABEL_HEIGHT);
+        sexLabel.setFont(this.labelFont);
+        sexLabel.setForeground(Color.WHITE);
         this.add(sexLabel);
 
         this.sexComboBox = new JComboBox<>(Constants.PASSENGER_SEX_OPTIONS);
         this.sexComboBox.setBounds(sexLabel.getX() + sexLabel.getWidth(), sexLabel.getY(), Constants.COMBO_BOX_WIDTH, Constants.COMBO_BOX_HEIGHT);
+        sexComboBox.setFont(this.labelFont);
         this.add(this.sexComboBox);
+
         this.sexChoice = this.sexComboBox.getItemAt(0);
         this.sexComboBox.addActionListener((e) -> {
             this.sexChoice = (String) this.sexComboBox.getSelectedItem();
@@ -143,68 +173,89 @@ public class MainPanel extends JPanel {
     private void createSibSpFilter() {
         JLabel sibSpLabel = new JLabel("Passenger number of siblings: ");
         sibSpLabel.setBounds(this.x, this.sexComboBox.getY() + Constants.COMBO_BOX_HEIGHT + Constants.SPACE, Constants.LABEL_WIDTH, Constants.LABEL_HEIGHT);
+        sibSpLabel.setFont(this.labelFont);
+        sibSpLabel.setForeground(Color.WHITE);
         this.add(sibSpLabel);
 
         this.sibSpTextField = new JTextField();
         this.sibSpTextField.setBounds(sibSpLabel.getX() + sibSpLabel.getWidth(), sibSpLabel.getY(), Constants.TEXT_FIELD_WIDTH, Constants.TEXT_FIELD_HEIGHT);
+        sibSpTextField.setFont(this.labelFont);
         this.add(this.sibSpTextField);
     }
 
     private void createParchFilter() {
         JLabel parchLabel = new JLabel("Passenger number of children / parents: ");
         parchLabel.setBounds(this.x, this.sibSpTextField.getY() + Constants.LABEL_HEIGHT + Constants.SPACE, Constants.LABEL_WIDTH, Constants.LABEL_HEIGHT);
+        parchLabel.setFont(this.labelFont);
+        parchLabel.setForeground(Color.WHITE);
         this.add(parchLabel);
 
         this.parchTextField = new JTextField();
         this.parchTextField.setBounds(parchLabel.getX() + parchLabel.getWidth(), parchLabel.getY(), Constants.TEXT_FIELD_WIDTH, Constants.TEXT_FIELD_HEIGHT);
+        parchTextField.setFont(this.labelFont);
         this.add(this.parchTextField);
     }
 
     private void createTicketFilter() {
         JLabel ticketLabel = new JLabel("Passenger number of ticket: ");
         ticketLabel.setBounds(this.x, this.parchTextField.getY() + Constants.LABEL_HEIGHT + Constants.SPACE, Constants.LABEL_WIDTH, Constants.LABEL_HEIGHT);
+        ticketLabel.setFont(this.labelFont);
+        ticketLabel.setForeground(Color.WHITE);
         this.add(ticketLabel);
 
         this.ticketTextField = new JTextField();
         this.ticketTextField.setBounds(ticketLabel.getX() + ticketLabel.getWidth(), ticketLabel.getY(), Constants.TEXT_FIELD_WIDTH, Constants.TEXT_FIELD_HEIGHT);
+        ticketTextField.setFont(this.labelFont);
         this.add(this.ticketTextField);
     }
 
     private void createFareFilter() {
         JLabel fareMinLabel = new JLabel("Min passenger ticket fare: ");
         fareMinLabel.setBounds(this.x, this.ticketTextField.getY() + Constants.LABEL_HEIGHT + Constants.SPACE, Constants.LABEL_WIDTH, Constants.LABEL_HEIGHT);
+        fareMinLabel.setFont(this.labelFont);
+        fareMinLabel.setForeground(Color.WHITE);
         this.add(fareMinLabel);
 
         this.fareMinTextField = new JTextField();
         this.fareMinTextField.setBounds(fareMinLabel.getX() + fareMinLabel.getWidth(), fareMinLabel.getY(), Constants.TEXT_FIELD_WIDTH, Constants.TEXT_FIELD_HEIGHT);
+        fareMinTextField.setFont(this.labelFont);
         this.add(this.fareMinTextField);
 
         JLabel fareMaxLabel = new JLabel("Max passenger ticket fare: ");
         fareMaxLabel.setBounds(this.x, this.fareMinTextField.getY() + Constants.LABEL_HEIGHT + Constants.SPACE, Constants.LABEL_WIDTH, Constants.LABEL_HEIGHT);
+        fareMaxLabel.setFont(this.labelFont);
+        fareMaxLabel.setForeground(Color.WHITE);
         this.add(fareMaxLabel);
 
         this.fareMaxTextField = new JTextField();
         this.fareMaxTextField.setBounds(fareMaxLabel.getX() + fareMaxLabel.getWidth(), fareMaxLabel.getY(), Constants.TEXT_FIELD_WIDTH, Constants.TEXT_FIELD_HEIGHT);
+        fareMaxTextField.setFont(this.labelFont);
         this.add(this.fareMaxTextField);
     }
 
     private void createCabinFilter() {
         JLabel cabinLabel = new JLabel("Passenger cabin number: ");
         cabinLabel.setBounds(this.x, this.fareMaxTextField.getY() + Constants.LABEL_HEIGHT + Constants.SPACE, Constants.LABEL_WIDTH, Constants.LABEL_HEIGHT);
+        cabinLabel.setFont(this.labelFont);
+        cabinLabel.setForeground(Color.WHITE);
         this.add(cabinLabel);
 
         this.cabinTextField = new JTextField();
         this.cabinTextField.setBounds(cabinLabel.getX() + cabinLabel.getWidth(), cabinLabel.getY(), Constants.TEXT_FIELD_WIDTH, Constants.TEXT_FIELD_HEIGHT);
+        cabinTextField.setFont(this.labelFont);
         this.add(this.cabinTextField);
     }
 
     private void createEmbarkedFilter() {
         JLabel embarkedLabel = new JLabel("Passenger embarked: ");
         embarkedLabel.setBounds(this.x, this.cabinTextField.getY() + Constants.LABEL_HEIGHT + Constants.SPACE, Constants.LABEL_WIDTH, Constants.LABEL_HEIGHT);
+        embarkedLabel.setFont(this.labelFont);
+        embarkedLabel.setForeground(Color.WHITE);
         this.add(embarkedLabel);
 
         this.embarkedComboBox = new JComboBox<>(Constants.PASSENGER_EMBARKED_OPTIONS);
         this.embarkedComboBox.setBounds(embarkedLabel.getX() + embarkedLabel.getWidth(), embarkedLabel.getY(), Constants.COMBO_BOX_WIDTH, Constants.COMBO_BOX_HEIGHT);
+        embarkedComboBox.setFont(this.labelFont);
         this.add(this.embarkedComboBox);
         this.embarkedChoice = this.embarkedComboBox.getItemAt(0).charAt(0);
         this.embarkedComboBox.addActionListener((e) -> {
@@ -215,13 +266,17 @@ public class MainPanel extends JPanel {
 
     private void createFilterButton() {
         this.filterButton = new JButton("Filter");
-        this.filterButton.setBounds(this.x, Constants.WINDOW_HEIGHT - 2 * (Constants.MARGIN_FROM_TOP + Constants.BUTTON_HEIGHT), Constants.BUTTON_WIDTH, Constants.BUTTON_HEIGHT);
+        this.filterButton.setBounds(Constants.WINDOW_WIDTH - (Constants.BUTTON_WIDTH + Constants.MARGIN_FROM_LEFT + 20 * Constants.SPACE), Constants.WINDOW_HEIGHT - 3 * (Constants.MARGIN_FROM_TOP + Constants.BUTTON_HEIGHT), Constants.BUTTON_WIDTH, Constants.BUTTON_HEIGHT);
+        filterButton.setFont(this.labelFont);
+        filterButton.setFont(this.buttonFont);
         this.add(this.filterButton);
     }
 
     private void createStatisticsButton() {
         this.statisticsButton = new JButton("Statistics");
-        this.statisticsButton.setBounds(Constants.WINDOW_WIDTH - (Constants.BUTTON_WIDTH + Constants.MARGIN_FROM_LEFT + 2 * Constants.SPACE), this.filterButton.getY(), Constants.BUTTON_WIDTH, Constants.BUTTON_HEIGHT);
+        this.statisticsButton.setBounds(filterButton.getX(), this.filterButton.getY()  + Constants.BUTTON_HEIGHT + Constants.SPACE , Constants.BUTTON_WIDTH, Constants.BUTTON_HEIGHT);
+        statisticsButton.setFont(this.labelFont);
+        statisticsButton.setFont(this.buttonFont);
         this.add(this.statisticsButton);
     }
 
